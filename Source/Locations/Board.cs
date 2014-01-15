ba062dbc5aada712eb35dab1855783e6e5d4cfa4
Praveen Angyan <praveen.angyan@gmail.com>
2014-01-15 11:36:24 -0800

    Adding in Board class

---
+++ Source/Locations/Board.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TurnItUp.Tmx;
+using System.Tuples;
+
+namespace TurnItUp.Locations
+{
+    public class Board
+    {
+        public Map Map { get; private set; }
+
+        public Board(string tmxPath)
+        {
+            Map = new Map(tmxPath);
+        }
+
+        public bool IsObstacle(int x, int y, int layer)
+        {
+            return (
+                // Is Layer present?
+                (Map.Layers.Count - 1) >= layer && 
+                // Does the layer have any properties?
+                Map.Layers[layer].Properties != null &&
+                // Is there a property for this layer called IsCollision?
+                Map.Layers[layer].Properties.ContainsKey("IsCollision") &&
+                // If so is the value true (this is a Collision layer)
+                Map.Layers[layer].Properties["IsCollision"] == "true" && 
+                // If any tile is present at coordinates x, y it is an obstacle
+                Map.Layers[layer].Tiles.ContainsKey(new Tuple<int,int>(x, y)));
+        }
+    }
+}
---
+++ Tests/Locations/BoardTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TurnItUp.Locations;
+
+namespace Tests.Locations
+{
+    [TestClass]
+    public class BoardTests
+    {
+        [TestMethod]
+        public void Board_ConstructionFromTmxTileMap_IsSuccessful()
+        {
+            Board board = new Board("../../Fixtures/FullExample.tmx");
+
+            Assert.IsNotNull(board.Map);
+        }
+
+        [TestMethod]
+        public void Board_DeterminingObstacles_ConsidersNoTilesAsObstaclesIfALayerDoesNotHaveATrueIsCollisionProperty()
+        {
+            Board board = new Board("../../Fixtures/FullExample.tmx");
+
+            // The example board has a "wall" around the entire 15x15 level
+            Assert.IsFalse(board.IsObstacle(0, 0, 0));
+            Assert.IsFalse(board.IsObstacle(0, 1, 0));
+            Assert.IsFalse(board.IsObstacle(1, 0, 0));
+            Assert.IsFalse(board.IsObstacle(2, 0, 0));
+        }
+
+        [TestMethod]
+        public void Board_DeterminingObstacles_TakesIntoAccountLayersHavingTrueForIsCollisionProperty()
+        {
+            Board board = new Board("../../Fixtures/FullExample.tmx");
+
+            // The example board has a "wall" around the entire 15x15 level
+            Assert.IsTrue(board.IsObstacle(0, 0, 1));
+            Assert.IsTrue(board.IsObstacle(0, 1, 1));
+            Assert.IsTrue(board.IsObstacle(1, 0, 1));
+            Assert.IsTrue(board.IsObstacle(2, 0, 1));
+
+            Assert.IsFalse(board.IsObstacle(1, 1, 1));
+        }
+    }
+}
