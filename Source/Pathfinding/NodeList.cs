1907e945efbac0b84908e8a7a63f70eab642f965
Praveen Angyan <praveen.angyan@gmail.com>
2014-01-16 01:35:38 -0800

    Adding NodeList for Pathfinding

---
+++ Source/Pathfinding/NodeList.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TurnItUp.Pathfinding
+{
+    public class NodeList : IList<Node>
+    {
+        private List<Node> list = new List<Node>();
+
+        public Node Find(Predicate<Node> match)
+        {
+            return list.Find(match);
+        }
+
+        public int IndexOf(Node item)
+        {
+            return list.IndexOf(item);
+        }
+
+        public void Insert(int index, Node item)
+        {
+            throw new NotImplementedException("Cannot insert at index; must preserve order.");
+        }
+
+        public void RemoveAt(int index)
+        {
+            list.RemoveAt(index);
+        }
+
+        public Node this[int index]
+        {
+            get
+            {
+                return list[index];
+            }
+            set
+            {
+                list.RemoveAt(index);
+                list.Add(value);
+            }
+        }
+
+        public void Add(Node item)
+        {
+            list.Add(item);
+            list.Sort(new NodeComparer());
+        }
+
+        public void Clear()
+        {
+            list.Clear();
+        }
+
+        public bool Contains(Node item)
+        {
+            return list.Contains(item);
+        }
+
+        public void CopyTo(Node[] array, int arrayIndex)
+        {
+            list.CopyTo(array, arrayIndex);
+        }
+
+        public int Count
+        {
+            get { return list.Count; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public bool Remove(Node item)
+        {
+            return list.Remove(item);
+        }
+
+        public IEnumerator<Node> GetEnumerator()
+        {
+            return list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return list.GetEnumerator();
+        }
+
+        private class NodeComparer : IComparer<Node>
+        {
+            public int Compare(Node node1, Node node2)
+            {
+                return node1.F.CompareTo(node2.F);
+            }
+        }
+    }
+}
---
+++ Tests/Pathfinding/NodeListTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TurnItUp.Pathfinding;
+using TurnItUp.Locations;
+using Tests.Factories;
+using System.Collections.Generic;
+
+namespace Tests.Pathfinding
+{
+    [TestClass]
+    public class NodeListTests
+    {
+        private Node _node;
+        private NodeList _nodeList;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _node = new Node(0, 0);
+            _nodeList = new NodeList();
+        }
+
+        [TestMethod]
+        public void NodeList_CanAddNewNode()
+        {
+            _nodeList.Add(_node);
+
+            Assert.AreEqual(1, _nodeList.Count);
+            Assert.AreEqual(_node, _nodeList[0]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void NodeList_WhenInserting_ThrowsException()
+        {
+            _nodeList.Insert(0, _node);
+        }
+
+        [TestMethod]
+        public void NodeList_WhenAddingNodes_AddsThemSortedByF()
+        {
+            Node node2 = new Node(5, 5);
+            Node node3 = new Node(5, 5);
+
+            _node.G = 10;
+            _node.H = 10;
+            node2.G = 5;
+            node2.H = 5;
+            node3.G = 3;
+            node3.H = 3;
+
+            _nodeList.Add(_node);
+            _nodeList.Add(node2);
+            _nodeList.Add(node3);
+
+            Assert.AreEqual(node3, _nodeList[0]);
+            Assert.AreEqual(node2, _nodeList[1]);
+            Assert.AreEqual(_node, _nodeList[2]);
+        }
+
+        [TestMethod]
+        public void NodeList_WhenAddingNodesWithSameF_AddsThemSuccessfully()
+        {
+            Node node2 = new Node(5, 5);
+            Node node3 = new Node(5, 5);
+
+            _node.G = 5;
+            _node.H = 5;
+            node2.G = 5;
+            node2.H = 5;
+            node3.G = 5;
+            node3.H = 5;
+
+            _nodeList.Add(_node);
+            _nodeList.Add(node2);
+            _nodeList.Add(node3);
+
+            Assert.IsTrue(_nodeList.Contains(_node));
+            Assert.IsTrue(_nodeList.Contains(node2));
+            Assert.IsTrue(_nodeList.Contains(node3));
+        }
+    }
+}
