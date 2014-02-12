3a44143d127a27026bc32991cb48ee394f22a11c
Praveen Angyan <praveen.angyan@gmail.com>
2014-02-12 14:26:07 -0800

    Starting the implementation of a SkillDatabase

---
+++ Tests/Skills/SkillDatabaseTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TurnItUp.Skills;
+using System.Collections.Generic;
+using System.Tuples;
+using TurnItUp.Pathfinding;
+using TurnItUp.Locations;
+using Tests.Factories;
+using TurnItUp.Components;
+
+namespace Tests.Skills
+{
+    [TestClass]
+    public class SkillDatabaseTests
+    {
+        [TestMethod]
+        public void SkillDatabase_CanAddASkill()
+        {
+        }
+    }
+}
