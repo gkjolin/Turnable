be50063b9f8858c644cd4a7aa4f569f081bb7036
Praveen Angyan <praveen.angyan@gmail.com>
2014-02-19 14:54:11 -0800

    Adding a basic SkillDatabase which can pool Skills for creating SkillSets

---
+++ Source/Skills/SkillDatabase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using TurnItUp.Components;
+using TurnItUp.Interfaces;
+using TurnItUp.Locations;
+
+namespace TurnItUp.Skills
+{
+    public class SkillDatabase : KeyedCollection<string, Skill>
+    {
+        public SkillSet CreateSkillSet(string[] skillNames)
+        {
+            SkillSet returnValue = new SkillSet();
+
+            foreach (string skillName in skillNames)
+            {
+                returnValue.Add(this[skillName]);
+            }
+
+            return returnValue;
+        }
+
+        protected override string GetKeyForItem(Skill skill)
+        {
+            return skill.Name;
+        }
+    }
+}
--- Tests/Skills/SkillDatabaseTests.cs
+++ Tests/Skills/SkillDatabaseTests.cs
@@ -13,9 +13,38 @@ namespace Tests.Skills
     [TestClass]
     public class SkillDatabaseTests
     {
+        private SkillDatabase _skillDatabase;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _skillDatabase = new SkillDatabase();
+        }
+
         [TestMethod]
         public void SkillDatabase_CanAddASkill()
         {
+            Skill skill = new Skill("Melee Attack");
+
+            _skillDatabase.Add(skill);
+
+            Assert.AreEqual(1, _skillDatabase.Count);
+            Assert.AreEqual(skill, _skillDatabase[0]);
+        }
+
+        [TestMethod]
+        public void SkillDatabase_GivenAnArrayOfSkillNames_CanCreateASkillSet()
+        {
+            Skill skill = new Skill("Melee Attack");
+            Skill skill2 = new Skill("Ranged Attack");
+            Skill skill3 = new Skill("Magic Attack");
+
+            _skillDatabase.Add(skill);
+            _skillDatabase.Add(skill2);
+            _skillDatabase.Add(skill3);
+
+            SkillSet skillSet = _skillDatabase.CreateSkillSet(new string[] {"Melee Attack", "Ranged Attack", "Magic Attack"});
+            Assert.AreEqual(3, skillSet.Count);
         }
     }
 }
