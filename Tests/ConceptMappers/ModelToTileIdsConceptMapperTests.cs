65b2cc66a86d608b4f63cd0bb6cda34d1871e0e7
Praveen Angyan <praveen.angyan@gmail.com>
2014-03-13 17:03:47 -0700

    Adding in first set of tests for concept mapper

--- Tests/ConceptMappers/ModelToTileIdsConceptMapperTests.cs
+++ Tests/ConceptMappers/ModelToTileIdsConceptMapperTests.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tests.Factories;
 
 namespace Tests.ConceptMappers
 {
     [TestClass]
     public class ModelToTileIdsConceptMapperTests
     {
+        private Board _board;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _board = LocationsFactory.BuildBoard();
+        }
+
         [TestMethod]
         public void ModelToTileIdsConceptMapper_CreatingAModelToTileIdsMapping_IsSuccessful()
         {
-
+            ModelToTileIdsConceptMapper mapper = new ModelToTileIdsConceptMapper();
         }
     }
 }
