072e1715423d7457f4f0ff825be3708fc7f432ae
Praveen Angyan <praveen.angyan@gmail.com>
2014-03-13 17:02:40 -0700

    Adding in Concept Mappers

---
+++ Tests/ConceptMappers/ModelToTileIdsConceptMapperTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.ConceptMappers
+{
+    [TestClass]
+    public class ModelToTileIdsConceptMapperTests
+    {
+        [TestMethod]
+        public void ModelToTileIdsConceptMapper_CreatingAModelToTileIdsMapping_IsSuccessful()
+        {
+
+        }
+    }
+}
