940d88701ec6609a31e8365a563008250e264570
Praveen Angyan <praveen.angyan@gmail.com>
2014-03-31 16:06:17 -0700

    Adding in basic structure of Area

---
+++ Source/Locations/Area.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TurnItUp.Tmx;
+using System.Tuples;
+using TurnItUp.Characters;
+using TurnItUp.Components;
+using TurnItUp.Interfaces;
+using Entropy;
+using TurnItUp.Pathfinding;
+using TurnItUp.Randomization;
+
+namespace TurnItUp.Locations
+{
+    public class Area
+    {
+        public List<Level> Levels { get; private set; }
+
+        public Area()
+        {
+            Levels = new List<Level>();
+        }
+    }
+}
---
+++ Tests/Locations/AreaTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TurnItUp.Locations;
+using TurnItUp.Interfaces;
+using Moq;
+using TurnItUp.Components;
+using System.Collections.Generic;
+using Entropy;
+using TurnItUp.Pathfinding;
+using TurnItUp.Tmx;
+using System.Tuples;
+
+namespace Tests.Locations
+{
+    [TestClass]
+    public class AreaTests
+    {
+        [TestMethod]
+        public void Area_Construction_IsSuccessful()
+        {
+            Area area = new Area();
+
+            Assert.IsNotNull(area.Levels);
+            Assert.IsNotNull(area.Connections);
+
+            Assert.IsFalse(true);
+        }
+    }
+}
