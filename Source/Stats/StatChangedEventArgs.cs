4cc7bbad49e933c569b7473cb8d9cc5bb18353eb
Praveen Angyan <praveen.angyan@gmail.com>
2015-01-12 16:58:30 -0800

    Adding in StatChangedEventArgs

---
+++ Source/Stats/StatChangedEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Turnable.Stats
+{
+    public class StatChangedEventArgs : EventArgs
+    {
+        public int OldValue { get; set; }
+        public int NewValue { get; set; }
+        public Stat Stat { get; set; }
+
+        public StatChangedEventArgs(Stat stat, int oldValue, int newValue)
+        {
+            Stat = stat;
+            OldValue = oldValue;
+            NewValue = newValue;
+        }
+    }
+}
---
+++ Tests/Stats/StatChangedEventArgsTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Turnable.Stats;
+using Turnable.Api;
+
+namespace Tests.Stats
+{
+    [TestClass]
+    public class StatChangedEventArgsTests
+    {
+        [TestMethod]
+        public void Constructor_InitializesAllProperties()
+        {
+            IStatManager statManager = new StatManager();
+            Stat stat = statManager.BuildStat("Health", 100);
+
+            StatChangedEventArgs statChangedEvent = new StatChangedEventArgs(stat, 100, 90);
+
+            Assert.AreEqual(stat, statChangedEvent.Stat);
+            Assert.AreEqual(100, statChangedEvent.OldValue);
+            Assert.AreEqual(90, statChangedEvent.NewValue);
+        }
+    }
+}
