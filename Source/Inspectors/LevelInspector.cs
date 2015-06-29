87ccf01bf1d83de0b41331b05baea8e45378c04a
Praveen Angyan <praveen.angyan@gmail.com>
2015-06-29 13:47:01 -0700

    Starting the creation of Inspectors

---
+++ Source/Inspectors/LevelInspector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Turnable.Api;
+using Turnable.Locations;
+
+namespace Turnable.Inspectors
+{
+    public class LevelInspector
+    {
+        public ILevel Level { get; set; }
+
+        public LevelInspector(ILevel level)
+        {
+            Level = level;
+        }
+    }
+}
---
+++ Tests/Inspectors/LevelInspectorTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Turnable.Api;
+using Tests.Factories;
+using Turnable.Inspectors;
+
+namespace Tests.Inspectors
+{
+    [TestClass]
+    public class LevelInspectorTests
+    {
+        private ILevel _level;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _level = LocationsFactory.BuildLevel();
+        }
+
+        [TestMethod]
+        public void Constructor_InitializesAllProperties()
+        {
+            LevelInspector levelInspector = new LevelInspector(_level);
+
+            Assert.AreEqual(_level, levelInspector.Level);
+        }
+    }
+}
