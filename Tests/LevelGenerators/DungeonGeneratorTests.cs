ec9bcf69a20677d6a6799978eb0a17428510a85a
Praveen Angyan <praveen.angyan@gmail.com>
2015-06-30 13:42:01 -0700

    Adding in Dungeon Generator

---
+++ Tests/LevelGenerators/DungeonGeneratorTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Turnable.LevelGenerators;
+
+namespace Tests.LevelGenerators
+{
+    [TestClass]
+    public class DungeonGeneratorTests
+    {
+        private DungeonGenerator _dungeonGenerator;
+
+        [TestMethod]
+        public void Chunk_GivenAStartingChunkOfTheMininumSizeAChunkCanBe_DoesNotCutItIntoChunks()
+        {
+        }
+    }
+}
