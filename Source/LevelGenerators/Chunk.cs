e9c357ef9f3d396c488fb889fd8edc3ed5eae186
Praveen Angyan <praveen.angyan@gmail.com>
2015-06-30 22:23:02 -0700

    Adding in the ability to split chunks

---
+++ Source/LevelGenerators/Chunk.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Turnable.Components;
+
+namespace Turnable.LevelGenerators
+{
+    public class Chunk
+    {
+        public Position Position { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public Chunk(Position position, int width, int height)
+        {
+            Position = position;
+            Width = width;
+            Height = height;
+        }
+
+        public List<Chunk> Split(SplitDirection splitDirection, int distance)
+        {
+            List<Chunk> splitChunks = new List<Chunk>();
+
+            switch(splitDirection)
+            {
+                case SplitDirection.Vertical:
+                    splitChunks.Add(new Chunk(Position, distance, Height));
+                    splitChunks.Add(new Chunk(new Position(Position.X + distance, Position.Y), Width - distance, Height));
+                    break;
+                case SplitDirection.Horizontal:
+                    splitChunks.Add(new Chunk(Position, Width, distance));
+                    splitChunks.Add(new Chunk(new Position(Position.X, Position.Y + distance), Width, Height - distance));
+                    break;
+            }
+
+            return splitChunks;
+        }
+    }
+}
---
+++ Tests/LevelGenerators/ChunkTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Turnable.Components;
+using Turnable.LevelGenerators;
+using System.Collections.Generic;
+
+namespace Tests.LevelGenerators
+{
+    [TestClass]
+    public class ChunkTests
+    {
+        [TestMethod]
+        public void Constructor_InitializesAllProperties()
+        {
+            Chunk chunk = new Chunk(new Position(0, 0), 5, 3);
+
+            Assert.AreEqual(new Position(0, 0), chunk.Position);
+            Assert.AreEqual(5, chunk.Width);
+            Assert.AreEqual(3, chunk.Height);
+        }
+
+        [TestMethod]
+        public void SplitChunk_WhenSplittingVertically_SplitsTheChunkIntoTwoChunksAlongAVerticalLineAtTheGivenDistance()
+        {
+            // *******
+            // *******
+            // New chunks:
+            // ***|****
+            // ***|****
+            Chunk initialChunk = new Chunk(new Position(0, 0), 7, 2);
+            List<Chunk> newChunks = initialChunk.Split(SplitDirection.Vertical, 3);
+
+            Assert.AreEqual(2, newChunks.Count);
+            Assert.AreEqual(new Position(0, 0), newChunks[0].Position);
+            Assert.AreEqual(3, newChunks[0].Width);
+            Assert.AreEqual(2, newChunks[0].Height);
+            Assert.AreEqual(new Position(3, 0), newChunks[1].Position);
+            Assert.AreEqual(4, newChunks[1].Width);
+            Assert.AreEqual(2, newChunks[1].Height);
+        }
+
+        [TestMethod]
+        public void SplitChunk_WhenSplittingHorizontally_SplitsTheChunkIntoTwoChunksAlongAHorizontalLineAtTheGivenDistance()
+        {
+            // *******
+            // *******
+            // *******
+            // New chunks:
+            // *******
+            // -------
+            // *******
+            // *******
+            Chunk initialChunk = new Chunk(new Position(0, 0), 7, 3);
+            List<Chunk> newChunks = initialChunk.Split(SplitDirection.Horizontal, 1);
+
+            Assert.AreEqual(2, newChunks.Count);
+            Assert.AreEqual(new Position(0, 0), newChunks[0].Position);
+            Assert.AreEqual(7, newChunks[0].Width);
+            Assert.AreEqual(1, newChunks[0].Height);
+            Assert.AreEqual(new Position(0, 1), newChunks[1].Position);
+            Assert.AreEqual(7, newChunks[1].Width);
+            Assert.AreEqual(2, newChunks[1].Height);
+        }
+
+    }
+}
