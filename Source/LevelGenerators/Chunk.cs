bf8b36c4806c9f012d8ab8a409969d16f8f2c879
Praveen Angyan <praveen.angyan@gmail.com>
2015-07-01 11:16:55 -0700

    Adding minimum size requirements when splitting chunks

--- Source/LevelGenerators/Chunk.cs
+++ Source/LevelGenerators/Chunk.cs
@@ -19,21 +19,47 @@ namespace Turnable.LevelGenerators
             Height = height;
         }
 
-        public List<Chunk> Split(SplitDirection splitDirection, int distance)
+        public List<Chunk> Split(SplitDirection splitDirection, int splitDistance, int minimumSplitChunkSize = 1)
         {
             List<Chunk> splitChunks = new List<Chunk>();
+
+            if (splitDistance < minimumSplitChunkSize) throw new ArgumentException();
 
             switch(splitDirection)
             {
                 case SplitDirection.Vertical:
-                    splitChunks.Add(new Chunk(Position, distance, Height));
-                    splitChunks.Add(new Chunk(new Position(Position.X + distance, Position.Y), Width - distance, Height));
+                    splitChunks.Add(new Chunk(Position, splitDistance, Height));
+                    splitChunks.Add(new Chunk(new Position(Position.X + splitDistance, Position.Y), Width - splitDistance, Height));
+
+                    // If any of the new chunks does not meet the minimumSplitChunkSize requirement, return an empty list.
+                    if (splitChunks[0].Width < minimumSplitChunkSize || splitChunks[1].Width < minimumSplitChunkSize)
+                    {
+                        splitChunks.Clear();
+                        return splitChunks;
+                    }
+
                     break;
                 case SplitDirection.Horizontal:
-                    splitChunks.Add(new Chunk(Position, Width, distance));
-                    splitChunks.Add(new Chunk(new Position(Position.X, Position.Y + distance), Width, Height - distance));
+                    if (splitDistance == Height) return splitChunks;
+                    splitChunks.Add(new Chunk(Position, Width, splitDistance));
+                    splitChunks.Add(new Chunk(new Position(Position.X, Position.Y + splitDistance), Width, Height - splitDistance));
+
+                    // If any of the new chunks does not meet the minimumSplitChunkSize requirement, return an empty list.
+                    if (splitChunks[0].Height < minimumSplitChunkSize || splitChunks[1].Height < minimumSplitChunkSize)
+                    {
+                        splitChunks.Clear();
+                        return splitChunks;
+                    }
+
                     break;
             }
+
+            // If any of the new chunks are invalid (have a width <= 0) return an empty list.
+            if (splitChunks[0].Width <= 0 || splitChunks[0].Height <= 0 || splitChunks[1].Height <= 0 || splitChunks[1].Width <= 0)
+            {
+                splitChunks.Clear();
+            }
+
 
             return splitChunks;
         }
--- Tests/LevelGenerators/ChunkTests.cs
+++ Tests/LevelGenerators/ChunkTests.cs
@@ -22,9 +22,10 @@ namespace Tests.LevelGenerators
         [TestMethod]
         public void SplitChunk_WhenSplittingVertically_SplitsTheChunkIntoTwoChunksAlongAVerticalLineAtTheGivenDistance()
         {
+            // Initial Chunk
             // *******
             // *******
-            // New chunks:
+            // New chunks
             // ***|****
             // ***|****
             Chunk initialChunk = new Chunk(new Position(0, 0), 7, 2);
@@ -42,6 +43,7 @@ namespace Tests.LevelGenerators
         [TestMethod]
         public void SplitChunk_WhenSplittingHorizontally_SplitsTheChunkIntoTwoChunksAlongAHorizontalLineAtTheGivenDistance()
         {
+            // Initial Chunk
             // *******
             // *******
             // *******
@@ -63,20 +65,78 @@ namespace Tests.LevelGenerators
         }
 
         [TestMethod]
-        public void SplitChunk_WhenAChunkCannotBeSplitVertically_ReturnsAnEmptyListOfChunks()
+        public void SplitChunk_WhenAChunkCannotBeSplitHorizontally_ReturnsAnEmptyListOfChunks()
         {
-            Chunk initialChunk = new Chunk(new Position(0, 0), 1, 3);
+            // Initial Chunk
+            // *******
+            Chunk initialChunk = new Chunk(new Position(0, 0), 7, 1);
             List<Chunk> newChunks = initialChunk.Split(SplitDirection.Horizontal, 1);
 
-            Assert.AreEqual(2, newChunks.Count);
-            Assert.AreEqual(new Position(0, 0), newChunks[0].Position);
-            Assert.AreEqual(7, newChunks[0].Width);
-            Assert.AreEqual(1, newChunks[0].Height);
-            Assert.AreEqual(new Position(0, 1), newChunks[1].Position);
-            Assert.AreEqual(7, newChunks[1].Width);
-            Assert.AreEqual(2, newChunks[1].Height);
+            Assert.AreEqual(0, newChunks.Count);
         }
 
+        [TestMethod]
+        public void SplitChunk_WhenAChunkCannotBeSplitVertically_ReturnsAnEmptyListOfChunks()
+        {
+            // Initial Chunk
+            // *
+            // *
+            // *
+            Chunk initialChunk = new Chunk(new Position(0, 0), 1, 3);
+            List<Chunk> newChunks = initialChunk.Split(SplitDirection.Vertical, 1);
 
+            Assert.AreEqual(0, newChunks.Count);
+        }
+
+        [TestMethod]
+        public void SplitChunk_GivenAMinimumHeightWhenBothSplitChunksCannotMeetRequirement_ReturnsAnEmptyListOfChunks()
+        {
+            // Initial Chunk
+            // *******
+            // *******
+            // *******
+            Chunk initialChunk = new Chunk(new Position(0, 0), 7, 3);
+            List<Chunk> newChunks = initialChunk.Split(SplitDirection.Horizontal, 2, 2);
+
+            Assert.AreEqual(0, newChunks.Count);
+        }
+
+        [TestMethod]
+        public void SplitChunk_GivenAMinimumWidthRequirementWhenBothSplitChunksCannotMeetRequirementReturnsAnEmptyListOfChunks()
+        {
+            // Initial Chunk
+            // ***
+            // ***
+            // ***
+            Chunk initialChunk = new Chunk(new Position(0, 0), 3, 3);
+            List<Chunk> newChunks = initialChunk.Split(SplitDirection.Vertical, 2, 2);
+
+            Assert.AreEqual(0, newChunks.Count);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SplitChunk_GivenAMinimumHeightWhenDistanceIsLessThanThisRequirement_ThrowsAnException()
+        {
+            // Initial Chunk
+            // *******
+            // *******
+            // *******
+            // *******
+            Chunk initialChunk = new Chunk(new Position(0, 0), 7, 4);
+            List<Chunk> newChunks = initialChunk.Split(SplitDirection.Horizontal, 1, 2);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SplitChunk_GivenAMinimumWidthRequirementWhenDistanceIsLessThanThisRequirement_ThrowsAnException()
+        {
+            // Initial Chunk
+            // ****
+            // ****
+            // ****
+            Chunk initialChunk = new Chunk(new Position(0, 0), 4, 3);
+            List<Chunk> newChunks = initialChunk.Split(SplitDirection.Vertical, 1, 2);
+        }
     }
 }
